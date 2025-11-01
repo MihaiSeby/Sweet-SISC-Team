8899ae7524ea6987e17b6289f921c1a442ed65a9
MihaiSeby <mihaiseby0613@gmail.com>
2025-11-01 23:48:10 +0200

    nav+hero+slider

--- FRONT-END/MAIN/script.js
+++ FRONT-END/MAIN/script.js
@@ -19,3 +19,15 @@ slider.innerHTML += slider.innerHTML;
 slider.innerHTML += slider.innerHTML;
 slider.innerHTML += slider.innerHTML;
 slider.innerHTML += slider.innerHTML;
+
+
+// Efect scroll pentru navbar
+const nav = document.querySelector("header");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
+  });
