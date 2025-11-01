9c3da51b0ca1bf132ccea3c47305116ef9a40625
MihaiSeby <mihaiseby0613@gmail.com>
2025-11-01 16:08:30 +0200

    3d commit

---
+++ FRONT-END/FORM/script.js
@@ -0,0 +1,17 @@
+const maxAllowed = 6; 
+    const checkboxes = document.querySelectorAll('input[name="option"]');
+
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', () => {
+        const checkedBoxes = document.querySelectorAll('input[name="option"]:checked');
+        if (checkedBoxes.length >= maxAllowed) {
+          checkboxes.forEach(box => {
+            if (!box.checked) {
+              box.disabled = true;
+            }
+          });
+        } else {
+          checkboxes.forEach(box => box.disabled = false);
+        }
+      });
+    });
